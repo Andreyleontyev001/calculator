a609a7b911f83cb4f098b03ffd074fe678be821b
Andrey <aleontev50338@gmail.com>
2022-07-12 21:04:04 +0200

    Изменил файлы для Никиты.

--- myjs.js
+++ myjs.js
@@ -1,8 +1,8 @@
 var op;
 function func() {
-var result;
 var num1 = Number(document.getElementById("num1").value);
 var num2 = Number(document.getElementById("num2").value);
+var result;
 switch (op) {
     case '+' :
         result = num1 + num2
